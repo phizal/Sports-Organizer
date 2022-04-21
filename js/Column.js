2a326dd78751e33cff7feb5b4daec6d48b1e9dd6
phizal <aphill81@vols.utk.edu>
2022-04-21 13:33:35 -0400

    update sprint 3

---
+++ js/Column.js
@@ -0,0 +1,18 @@
+var select = document.getElementById('sports');
+var val = select.options[select.selectedIndex].value;
+var table = document.getElementById(val);
+var col5 = document.getElementById('col5');
+col5.innerHTML = table.rows[0].cells[6].innerHTML;
+
+document.addEventListener('input', function (event) {
+
+	if (event.target.id !== 'sports') return;
+    else {
+		select = document.getElementById('sports');
+        val = select.options[select.selectedIndex].value;
+        table = document.getElementById(val);
+        col5 = document.getElementById('col5');
+        col5.innerHTML = table.rows[0].cells[6].innerHTML;
+	}
+}, false);
+
