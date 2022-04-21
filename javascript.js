e2ff368f9cabf9d9e5c30379c849cf69f4606ae9
phizal <aphill81@vols.utk.edu>
2022-04-21 13:29:18 -0400

    sprint 3

--- javascript.js
+++ javascript.js
@@ -2,11 +2,58 @@
     document.getElementById('basketball').style.display = "none";
 }*/
 
+//download function from: https://attacomsian.com/blog/javascript-download-file
+const download = (path, filename) => {
+    // Create a new link
+    const anchor = document.createElement('a');
+    anchor.href = path;
+    anchor.download = filename;
 
+    // Append to the DOM
+    document.body.appendChild(anchor);
 
-function updateSport() {
+    // Trigger `click` event
+    anchor.click();
+
+    // Remove element from DOM
+    document.body.removeChild(anchor);
+};
+
+function save(){
+    var select = document.getElementById("sports");
+    var val = select.options[select.selectedIndex].value;
+    var table = document.getElementById(val);
+    var saveArr = new Array(rowTot+1);
+
+    for(var i = 0; i < rowTot+1; i++){
+        //populate array
+        saveArr[i] = new Array(colTot-2);
+        for(var j = 0; j < colTot-2; j++)
+        {
+            saveArr[i][j] = table.rows[i].cells[j+2].innerHTML;
+            console.log(table.rows[i].cells[j+2].innerHTML);
+        }
+    }
+    //turn saveArr into JSON and save to local storage
+    const saveFile = JSON.stringify(saveArr);
+    console.log(saveFile);
+    //create data blob
+    const blob = new Blob([saveFile], {type : 'application/json'});
+    //create download link
+    const url = URL.createObjectURL(blob);
+    //start download
+    download(url, "SportsOrganizerDownload.JSON");
+    //clean up
+    URL.revokeObjectURL(url);
+}
+
+/*function updateSport() {
+    
+
     var sportddl = document.getElementById("sports");
     var sport = sportddl.options[sportddl.selectedIndex].text;   
+    
+
 
    /* if (sport == "Basketball")
     {
@@ -16,9 +63,9 @@ function updateSport() {
     else{
         document.getElementById('golf').style.display = '';
         document.getElementById('basketball').style.display = "none";
-    }*/
+    }
     alert(sport + " has been selected!");
-}
+}*/
 
 function updateTeam() {
     var teamddl = document.getElementById("teams");
@@ -29,12 +76,12 @@ function updateTeam() {
 
 
  
-function delButton(row){
+function delButton(row, tableid){
     var btn = document.createElement('button');
     btn.type = "delBtn";
     btn.className = "btn";
     btn.innerHTML= "X";
-    btn.addEventListener("click", function() { delPlayApp(row.id); });
+    btn.addEventListener("click", function() { delPlayApp(row.id,tableid); });
     btn.style.textAlign = "center";
     var tdbtn = document.createElement("td");
     tdbtn.appendChild(btn);
@@ -42,28 +89,28 @@ function delButton(row){
     return row;
 }
 
-function delPlayApp(idVal) {
-    var table = document.getElementById('players');
+function delPlayApp(idVal,tableid) {
+    var table = document.getElementById(tableid);
     for (i = 1; i < table.rows.length; i++) {
         currRow = table.rows[i];   
         if(currRow.id == idVal){
-            document.getElementById("players").deleteRow(i);
+            document.getElementById(tableid).deleteRow(i);
             break;
         }
     }
 }
 
-function delPlay(r) {
+function delPlay(r,tableid) {
     var i = r.parentNode.parentNode.rowIndex;
-    document.getElementById("players").deleteRow(i);
+     document.getElementById(tableid).deleteRow(i);
 }
 
-function editButton(row){
+function editButton(row,tableid){
     var btn = document.createElement('button');
     btn.type = "editBtn";
     btn.className = "btn";
     btn.innerHTML= "Edit";
-    btn.addEventListener("click", function() { editPlayApp(row.id); });
+    btn.addEventListener("click", function() { editPlayApp(row.id,tableid); });
     btn.style.textAlign = "center";
     var tdbtn = document.createElement("td");
     tdbtn.appendChild(btn);
@@ -71,17 +118,17 @@ function editButton(row){
     return row;
 }
 
-function editPlay(r) {
-    var table = document.getElementById('players');
+function editPlay(r,tableid) {
+    var table = document.getElementById(tableid);
     var row = r.parentNode.parentNode.rowIndex;
 
-    for(var i = 1; i < table.rows[0].cells.length-1; i+=1){
+    for(var i = 2; i < table.rows[0].cells.length; i+=1){
         var initVal = table.rows[row].cells[i].innerHTML;
         var newVal = prompt(table.rows[0].cells[i].innerHTML, initVal);
         if (newVal == null || newVal == "") {
             console.log("User cancelled the prompt.");
             break;
-        } else if(i == 3){
+        } else if(i == 4){
             newVal = numValid(newVal);
             if(newVal != null && newVal != ""){
                 table.rows[row].cells[i].innerHTML = newVal;
@@ -92,19 +139,19 @@ function editPlay(r) {
     }
 }
 
-function editPlayApp(idVal) {
-    var table = document.getElementById('players');
+function editPlayApp(idVal,tableid) {
+    var table = document.getElementById(tableid);
     for (i = 1; i < table.rows.length; i++) {
         currRow = table.rows[i];  
         if(currRow.id == idVal){
-            for(var j = 1; j < table.rows[0].cells.length-1; j+=1){
+            for(var j = 2; j < table.rows[0].cells.length; j+=1){
                 var initVal = table.rows[i].cells[j].innerHTML;
                 var newVal = prompt(table.rows[0].cells[j].innerHTML, initVal);
                 if (newVal == null || newVal == "") {
                     console.log("User cancelled the prompt.");
                     break;
                 }
-                else if(j == 3){
+                else if(j == 4){
                     newVal = numValid(newVal);
                     if(newVal != null && newVal != ""){
                         table.rows[i].cells[j].innerHTML = newVal;
@@ -154,10 +201,14 @@ var rowTot = 3;
 
 function addRow() {
     "use strict";
+
+    var select = document.getElementById('sports');
+    var val = select.options[select.selectedIndex].value;
  
     rowTot += 1;
+    
 
-    var table = document.getElementById("players");
+    var table = document.getElementById(val);
     var row = document.createElement("tr");
     var td1 = document.createElement("td");
     var td2 = document.createElement("td");
@@ -165,17 +216,11 @@ function addRow() {
     var td4 = document.createElement("td");
     var td5 = document.createElement("td");
 
-    let fname = prompt("Enter first name");
-    let lname = prompt("Enter last name");
-    let age = prompt("Enter age");
-    let position = prompt("Enter position");
-    let location = prompt("Enter location");
-
-    td1.innerHTML  = fname;
-    td2.innerHTML  = lname;
-    td3.innerHTML  = age;
-    td4.innerHTML  = location;
-    td5.innerHTML  = position;
+    td1.innerHTML  = document.getElementById("fname").value;
+    td2.innerHTML  = document.getElementById("lname").value;
+    td3.innerHTML  = document.getElementById("age").value;
+    td4.innerHTML  = document.getElementById("location").value;
+    td5.innerHTML  = document.getElementById("col5in").value;
 
     var rowArray = [td1,td2,td3,td4,td5];
     for(var k = 0; k < rowArray.length; k++){
@@ -185,21 +230,39 @@ function addRow() {
         }
     }
 
+    document.getElementById("fname").value = "";
+    document.getElementById("lname").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("location").value = "";
+    document.getElementById("col5in").value = "";
+
     for(k = 0; k < rowArray.length; k++){
         rowArray[k].style.textAlign = "center";
     }
 
-    row.id = "row." + rowTot;
+    row.id = val +  "." + rowTot;
 
-    row = editButton(row);
+    row = editButton(row,val);
+    row = delButton(row,val);
 
     row.appendChild(td1);
     row.appendChild(td2);
     row.appendChild(td3);
     row.appendChild(td4);
     row.appendChild(td5);
-    
-    row = delButton(row);
+
+    var int2 = 0;
+    var loop = 7;
+    while(loop < colTot){
+        var td6 = document.createElement("td");
+        var currCol = "val" + int2;
+        td6.innerHTML = document.getElementById(currCol).value
+        td6.style.textAlign = "center";
+        document.getElementById(currCol).value = "";
+        int2++;
+        row.appendChild(td6);
+        loop++;
+    }
     
     table.children[0].appendChild(row);
 
@@ -226,22 +289,31 @@ function checkNum(currRow){
     return false;
 }
 
-var colTot = 4;
+var colTot = 7;
 
 function addColumn() {
     "use strict";
 
-    colTot += 1;
+    var select = document.getElementById('sports');
+    var val = select.options[select.selectedIndex].value;
 
-    var table = document.getElementById("players");
-    var colHead = table.rows[0].insertCell(colTot)
-    colHead.innerHTML = document.getElementById("addColtxt").value;
+    colTot += 1;
+    var newCol = document.getElementById("addColtxt").value;
+    if(newCol == ""){
+        return;
+    }
+    var table = document.getElementById(val);
+    var colHead = table.rows[0].insertCell(-1)
+    colHead.innerHTML = newCol;
+    colHead.className = "cellInsert";
     document.getElementById("addColtxt").value = "";
 
-    for (i = 1; i < table.rows.length; i++) {
-        var temp = table.rows[i].insertCell(colTot)
-        temp.innerHTML = document.getElementById("addColtxt").value;
+    for (var i = 1; i < table.rows.length; i++) {
+        var temp = table.rows[i].insertCell(-1);
+        temp.className = "cellInsert";
+        temp.innerHTML = "N/A";
     }
+    console.log(colTot);
 };
 
 function darkModeFunc() {
